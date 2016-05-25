06892cbdf00e353dbc9fe0d2b64fbc4b2a049aa2
Daniel Gomez <gole1407Qgmail.com>
2016-05-25 12:40:34 -0500

    Eliminar usuario (19)

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -454,25 +454,25 @@ class Main extends CI_Controller {
             $checkDel = $this->user_model->deleteUser($this->input->post('id'));
             if($checkDel){
                 $data = $this->session->userdata;
-                $data['message'] = '1';
+                $mensaje = '1';
 
                 if ($data['tipo'] === '1') {
 
                     $data['estu'] = $this->user_model->getUsersList();
                     
                     $this->load->view('header', $data);
-                    $this->load->view('home_view_admin',$data); 
+                    $this->load->view('home_view_admin',$mensaje); 
                     $this->load->view('footer');
             }else{
                 $data = $this->session->userdata;
-                $data['message'] = '2';
+                $mensaje = '2';
 
                 if ($data['tipo'] === '1') {
 
                     $data['estu'] = $this->user_model->getUsersList();
                     
                     $this->load->view('header', $data);
-                    $this->load->view('home_view_admin',$data); 
+                    $this->load->view('home_view_admin',$mensaje); 
                     $this->load->view('footer');
             }
 
