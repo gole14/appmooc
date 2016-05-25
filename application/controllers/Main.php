0ddd2f11dedf49678b4c7104174dc07dff9bf016
Daniel Gomez <gole1407Qgmail.com>
2016-05-25 12:30:46 -0500

    Eliminar usuario (16)

--- application/controllers/Main.php
+++ application/controllers/Main.php
@@ -431,7 +431,7 @@ class Main extends CI_Controller {
 
                 $data['estu'] = $this->user_model->getUsersList();
                 $this->load->view('header', $data);
-                $this->load->view('home_view_admin',$message); 
+                $this->load->view('home_view_admin'); 
                 $this->load->view('footer');
 
 
