7f5748214e3d84f305d26b39181e82d94ffc339a
Daniel Gomez <gole1407Qgmail.com>
2016-05-25 13:19:23 -0500

    Eliminar usuario (24)

--- application/views/delete_ok.php
+++ application/views/delete_ok.php
@@ -6,11 +6,19 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <head>
 	<meta charset="utf-8">
 	<title>Area privada</title>
+    <link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+    <link rel="stylesheet" type="text/css" media="all" href="<?php echo base_url(); ?>assets/css/style.css" />
+
+    <meta http-equiv="refresh" content="5;url=<?php site_url().'/main/admin' ?>">
+
+    
+
+
 </head>
 <body>
 <div class="hero-unit">
 	<div class="alert alert-success" role="alert">
-  <strong>Well done!</strong> You successfully read this important alert message.
+  		<strong>Well done!</strong> You successfully read this important alert message.
 </div>
 </div>
 </body>
