2157c98efabcf3a72e85cf7131936bb8b4085f10
Daniel Gomez <gole1407Qgmail.com>
2016-05-30 03:27:46 -0500

    Subir video(5)

--- application/views/page_curso_unico.php
+++ application/views/page_curso_unico.php
@@ -15,12 +15,12 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 
 
 	<div id="blog_text">
-		<iframe width="560" height="315" src="https://www.youtube.com/embed/kY5P9sZqFas" frameborder="0" allowfullscreen></iframe>
+		<iframe width="560" height="315" src="<?php echo $url ?>" frameborder="0" allowfullscreen></iframe>
 	</div>
 
 	<?php echo form_open('main/insert_ctrl'); ?>
 
-	<input type="hidden" value="1" name="idcurso">
+	<input type="hidden" value="<?php echo $idc ?>" name="idcurso">
 	
 	<?php echo form_submit(array('id' => 'submit', 'value' => 'Registrarse al curso', 'class' => 'btn btn-primary btn-lg')); ?>
 	<?php echo form_close(); ?><br/>
